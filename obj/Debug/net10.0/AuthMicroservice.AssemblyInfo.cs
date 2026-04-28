bce98ac35cb07279989d299a13b6396200ce999f
Guilherme Alberto <guilhermecamelo86@gmail.com>
2026-04-28 08:41:59 -0300

    fix compose port

--- obj/Debug/net10.0/AuthMicroservice.AssemblyInfo.cs
+++ obj/Debug/net10.0/AuthMicroservice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthMicroservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f9d46ffa55d4fd8de98d10cce757f7f08d98779")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthMicroservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthMicroservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
